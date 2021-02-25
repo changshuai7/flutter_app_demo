d1dc09902b5f6ac0a84ad493ff8d010808891504
常帅 <changshuai@renrenche.com>
2021-02-25 18:29:33 +0800

    删除my_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_demo/data/list_data.dart';
-import 'package:flutter_app_demo/my_test.dart';
 import 'package:flutter_app_demo/widget/center_demo.dart';
 import 'package:flutter_app_demo/widget/container_and_text_demo.dart';
 import 'package:flutter_app_demo/widget/row_column_expanded_demo.dart';
