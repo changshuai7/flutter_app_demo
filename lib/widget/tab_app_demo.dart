a6ffbc416ca8bd9a83bae1e31c41d04cc1212fc6
常帅 <changshuai@renrenche.com>
2021-03-01 18:15:36 +0800

    bottomNavigationBar 代码修改

--- lib/widget/tab_app_demo.dart
+++ lib/widget/tab_app_demo.dart
@@ -9,10 +9,9 @@ class _TabAppDemoState extends State<TabAppDemo> {
   int _currentIndex = 0;
   List<Widget> _pageList = [TabHome(), TabMine(), TabSetting()];
   List<BottomNavigationBarItem> _barList = [
-    BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("首页")),
-    BottomNavigationBarItem(icon: Icon(Icons.person), title: Text("我的")),
-    BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("设置")),
-
+    BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("首页"), backgroundColor: Colors.red),
+    BottomNavigationBarItem(icon: Icon(Icons.person), title: Text("我的"), backgroundColor: Colors.blue),
+    BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("设置"), backgroundColor: Colors.green),
   ];
 
   @override
@@ -25,6 +24,7 @@ class _TabAppDemoState extends State<TabAppDemo> {
         body: this._pageList[_currentIndex],
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
+          type: BottomNavigationBarType.shifting,
           items: _barList,
           onTap: (index) {
             setState(() {
