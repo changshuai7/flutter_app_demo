8123abfbb51e8dff36f69403dbc1ef4699acfaf8
常帅 <changshuai@renrenche.com>
2021-03-07 20:18:25 +0800

    Button done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_demo/data/list_data.dart';
 import 'package:flutter_app_demo/widget/aspect_ratio.dart';
+import 'package:flutter_app_demo/widget/button_demo.dart';
 import 'package:flutter_app_demo/widget/card_demo.dart';
 import 'package:flutter_app_demo/widget/center_demo.dart';
 import 'package:flutter_app_demo/widget/container_and_text_demo.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("MyFlutter"),
         ),
-        body: STFullDemo3(),
+        body: ButtonDemo(),
       ),
       theme: ThemeData(
         primarySwatch: Colors.green,
---
+++ lib/widget/button_demo.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+class ButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        children: [
+          Text(
+            '-------- Button演示 --------',
+            style: TextStyle(color: Colors.red),
+          ),
+          BtnShow(),
+          Text(
+            '-------- Button带图标演示 --------',
+            style: TextStyle(color: Colors.red),
+          ),
+          BtnShowWithIcon(),
+          Text(
+            '-------- Button测试 --------',
+            style: TextStyle(color: Colors.red),
+          ),
+          BtnTest(),
+        ],
+      ),
+    );
+  }
+}
+
+class BtnShow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        RaisedButton(
+          onPressed: () {},
+          child: Text('RaisedButton'),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        FlatButton(
+          onPressed: () {},
+          child: Text('FlatButton'),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        OutlineButton(
+          onPressed: () {},
+          child: Text('OutlineButton'),
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.thumb_up),
+        ),
+      ],
+    );
+  }
+}
+
+class BtnShowWithIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        RaisedButton.icon(
+          icon: Icon(Icons.send),
+          label: Text("发送"),
+          onPressed: () {},
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        OutlineButton.icon(
+          icon: Icon(Icons.add),
+          label: Text("添加"),
+          onPressed: () {},
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        FlatButton.icon(
+          icon: Icon(Icons.info),
+          label: Text("详情"),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
+
+class BtnTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 200,
+          height: 50,
+          child: RaisedButton(
+            color: Colors.blue,
+            textColor: Colors.yellow,
+            disabledColor: Colors.grey,
+            splashColor: Colors.green,
+            highlightColor: Colors.black,
+            padding: EdgeInsets.all(10),
+            shape: RoundedRectangleBorder(
+                side: BorderSide(color: Colors.red, width: 3),
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            onPressed: () {},
+            child: Text('我是一个Button'),
+          ),
+        ),
+        SizedBox(height: 10),
+        Container(
+          width: 100,
+          height: 100,
+          child: RaisedButton(
+            color: Colors.blue,
+            textColor: Colors.yellow,
+            disabledColor: Colors.grey,
+            splashColor: Colors.green,
+            highlightColor: Colors.black,
+            padding: EdgeInsets.all(10),
+            shape: CircleBorder(side: BorderSide(color: Colors.red, width: 3)),
+            onPressed: () {},
+            child: Text(
+              '我是一个Button',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
