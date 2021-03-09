f14578f0d707d836e5cb6ae28a4d951a520f24f8
常帅 <changshuai@renrenche.com>
2021-03-09 17:36:57 +0800

    RadioListTile done

--- lib/widget/radio_demo.dart
+++ lib/widget/radio_demo.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("RadioDemo"),
         ),
-        body: RadioDemo(),
+        body: RadioListTileDemo(),
       ),
     );
   }
@@ -63,6 +63,64 @@ class _RadioDemoState extends State<RadioDemo> {
           Text('结果为：${radioValue == null ? '未选择' : radioValue}'),
         ],
       ),
+    );
+  }
+}
+
+class RadioListTileDemo extends StatefulWidget {
+  @override
+  _RadioListTileDemoState createState() => _RadioListTileDemoState();
+}
+
+class _RadioListTileDemoState extends State<RadioListTileDemo> {
+  String radioValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue, width: 1),
+          ),
+          child: RadioListTile(
+            groupValue: radioValue,
+            value: '男',
+            onChanged: (v) {
+              setState(() {
+                radioValue = v;
+              });
+            },
+            activeColor: Colors.green,
+            title: Text('男'),
+            subtitle: Text('我是一个副标题'),
+            secondary: Icon(Icons.home),
+            selected: radioValue == '男',
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue, width: 1),
+          ),
+          child: RadioListTile(
+            groupValue: radioValue,
+            value: '女',
+            onChanged: (v) {
+              setState(() {
+                radioValue = v;
+              });
+            },
+            activeColor: Colors.green,
+            title: Text('女'),
+            subtitle: Text('我是一个副标题'),
+            secondary: Icon(Icons.home),
+            selected: radioValue == '女',
+          ),
+        ),
+        Text('结果为：${radioValue == null ? '未选择' : radioValue}'),
+      ],
     );
   }
 }
